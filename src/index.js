86eb44350c179fd97298b37a541ba21d7a242047
Bassem Mohamed <bassemmohamed1994@gmail.com>
2019-12-18 00:14:32 +0200

    Initial setup for redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import WeatherApp from "./WeatherApp";
+import store from "./store";
 
-ReactDOM.render(<WeatherApp />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <WeatherApp />
+  </Provider>,
+  document.getElementById("root")
+);
