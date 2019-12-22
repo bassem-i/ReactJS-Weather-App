369dde16e813715385ccb94bf379b41c07fe1b8f
Bassem Mohamed <bassemmohamed1994@gmail.com>
2019-12-22 12:54:15 +0200

    Change API request to https

--- src/service/apiService.js
+++ src/service/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const WeatherURL =
-  "http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40";
+  "https://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40";
 
 function getWeatherInfo() {
   return axios.get(WeatherURL);
